ecf8b8829e4a78fe8112b0fe88a9d93e27992ef5
mercy <mcherono529@gmail.com>
2024-10-06 17:47:13 +0300

    adds mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  let heading2 = document.querySelector("h2");
+  
+  heading2.addEventListener("mouseover", () => {
+    heading2.style.color = "red";
+  });
+
+  heading2.addEventListener("mouseleave", () => {
+    heading2.style.color = "black";
+  });
+
+  let form = document.getElementById('create-task-form');
+  let taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let taskInput = document.getElementById('new-task-description');
+    let taskDescription = taskInput.value.trim(); 
+
+    if (taskDescription) {
+      let taskItem = document.createElement('li');
+      taskItem.textContent = taskDescription;
+
+      let button = document.createElement('button');
+      button.textContent = 'X';
+      button.className = 'delete-btn';
+      button.style.marginLeft = "10px"; 
+      button.addEventListener('click', () => {
+        taskItem.remove(); 
+      });
+
+      taskItem.appendChild(button);
+      taskList.appendChild(taskItem);
+
+      taskInput.value = '';
+    } else {
+      alert('Please enter a task description.'); 
+    }
+  });
 });
